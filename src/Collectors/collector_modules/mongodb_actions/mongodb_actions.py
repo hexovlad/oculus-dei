984c5cf164d95cc4e879f872b7be8b846eef99bb
hexovlad <48881425+hexovlad@users.noreply.github.com>
2024-04-19 21:56:32 +0300

    Linted files

--- src/Collectors/collector_modules/mongodb_actions/mongodb_actions.py
+++ src/Collectors/collector_modules/mongodb_actions/mongodb_actions.py
@@ -2,13 +2,16 @@ from pymongo import MongoClient
 
 
 class DatabaseConnector:
-    def __init__(self, database_url: str,
-                 database_port: int,
-                 database_name: str,
-                 database_user: str = None,
-                 database_password: str = None,
-                 database_location: str = None,
-                 database_auth_name: str = None):
+    def __init__(
+        self,
+        database_url: str,
+        database_port: int,
+        database_name: str,
+        database_user: str = None,
+        database_password: str = None,
+        database_location: str = None,
+        database_auth_name: str = None,
+    ):
         # Getting the access for initiating the database connection
         self._DATABASE_HOST = database_url
         self._DATABASE_PORT = database_port
@@ -23,19 +26,25 @@ class DatabaseConnector:
 
         self.connection_established = False
 
-        self.connection_string = self._create_connection_string()  # Creating the connection string
+        self.connection_string = (
+            self._create_connection_string()
+        )  # Creating the connection string
 
         self.connect_to_database()  # Connecting to the database
 
     def _create_connection_string(self) -> str or None:
         """Makes the connection string for the database"""
-        if self._DATABASE_USER is not None and self._DATABASE_PASSWORD is not None and self._DATABASE_AUTH_NAME is None:
+        if (
+            self._DATABASE_USER is not None
+            and self._DATABASE_PASSWORD is not None
+            and self._DATABASE_AUTH_NAME is None
+        ):
             """Authentication in case the user and password is provided"""
-            return f'mongodb://{self._DATABASE_USER}:{self._DATABASE_PASSWORD}@{self._DATABASE_HOST}:{self._DATABASE_PORT}/{self._DATABASE_NAME}'
+            return f"mongodb://{self._DATABASE_USER}:{self._DATABASE_PASSWORD}@{self._DATABASE_HOST}:{self._DATABASE_PORT}/{self._DATABASE_NAME}"
 
         elif self._DATABASE_AUTH_NAME is not None:
             """Authentication in case the auth database is specified"""
-            return f'mongodb://{self._DATABASE_USER}:{self._DATABASE_PASSWORD}@{self._DATABASE_HOST}:{self._DATABASE_PORT}/{self._DATABASE_NAME}?authSource={self._DATABASE_AUTH_NAME}'
+            return f"mongodb://{self._DATABASE_USER}:{self._DATABASE_PASSWORD}@{self._DATABASE_HOST}:{self._DATABASE_PORT}/{self._DATABASE_NAME}?authSource={self._DATABASE_AUTH_NAME}"
 
         else:
             """Returning None in the case of basic auth"""
@@ -54,12 +63,14 @@ class DatabaseConnector:
 
             self.db = self.db_client[self._DATABASE_NAME]
             # Logging us connecting to the database
-            print(f'\n====================\nConnected to MongoDB\n====================\n')
+            print(
+                f"\n====================\nConnected to MongoDB\n====================\n"
+            )
             self.connection_established = True  # Setting the connection to established
 
         except ConnectionError as e:
             """Throwing an error in case the database doesn't connect"""
-            print('There was an error while connecting to MongoDB: ', str(e))
+            print("There was an error while connecting to MongoDB: ", str(e))
 
     def disconnect(self):
         """Disconnects from the database"""
@@ -85,11 +96,11 @@ class DatabaseActions:
             result = db_collection.insert_one(data)
 
             if result.inserted_id:
-                print('Document inserted successfully.')
+                print("Document inserted successfully.")
             else:
-                print('Failed to insert document.')
+                print("Failed to insert document.")
         except Exception as e:
-            print('Error inserting document:', str(e))
+            print("Error inserting document:", str(e))
 
     def data_insert_many(self, collection: str, data):
         """Gets the desired information"""
@@ -98,11 +109,11 @@ class DatabaseActions:
             result = db_collection.insert_many(data)
 
             if result.inserted_id:
-                print('Document inserted successfully.')
+                print("Document inserted successfully.")
             else:
-                print('Failed to insert document.')
+                print("Failed to insert document.")
         except Exception as e:
-            print('Error inserting document:', str(e))
+            print("Error inserting document:", str(e))
 
     def data_get(self, collection: str, query: dict) -> dict or None:
         """Getting data based on a query"""
@@ -114,9 +125,14 @@ class DatabaseActions:
             return result
 
         except Exception as e:
-            print(f'Error while looking for the following in the database.\n QUERY:\n{query}\n ', str(e))
+            print(
+                f"Error while looking for the following in the database.\n QUERY:\n{query}\n ",
+                str(e),
+            )
 
-        return None  # Returning None in case there is no information found in the database
+        return (
+            None  # Returning None in case there is no information found in the database
+        )
 
     def data_get_many(self, collection: str, query: dict) -> dict or list or None:
         """Getting data based on a query"""
@@ -128,33 +144,46 @@ class DatabaseActions:
             return result
 
         except Exception as e:
-            print(f'Error while looking for the following in the database.\n QUERY:\n{query}\n ', str(e))
+            print(
+                f"Error while looking for the following in the database.\n QUERY:\n{query}\n ",
+                str(e),
+            )
 
-        return None  # Returning None in case there is no information found in the database
+        return (
+            None  # Returning None in case there is no information found in the database
+        )
 
-    def data_modify_one(self, collection: str, filter_query: dict, updated_data: dict) -> str or None:
+    def data_modify_one(
+        self, collection: str, filter_query: dict, updated_data: dict
+    ) -> str or None:
         """Modifies the data on a given key"""
         try:
             db_collection = self.db_connector.db[collection]
-            result = db_collection.update_one(filter_query, updated_data)  # Updating the entry based on the filter
+            result = db_collection.update_one(
+                filter_query, updated_data
+            )  # Updating the entry based on the filter
 
             return result
 
         except Exception as e:
-            print(f'Error while updating the database.', str(e))
+            print(f"Error while updating the database.", str(e))
 
         return None
 
-    def data_modify_many(self, collection: str, filter_query: dict, updated_data: dict) -> str or None:
+    def data_modify_many(
+        self, collection: str, filter_query: dict, updated_data: dict
+    ) -> str or None:
         """Modifies the data on a given key"""
         try:
             db_collection = self.db_connector.db[collection]
-            result = db_collection.update_many(filter_query, updated_data)  # Updating the entry based on the filter
+            result = db_collection.update_many(
+                filter_query, updated_data
+            )  # Updating the entry based on the filter
 
             return result
 
         except Exception as e:
-            print(f'Error while updating many things in the database.', str(e))
+            print(f"Error while updating many things in the database.", str(e))
 
         return None
 
@@ -162,12 +191,14 @@ class DatabaseActions:
         """Deletes the data on a given key"""
         try:
             db_collection = self.db_connector.db[collection]
-            result = db_collection.delete_one(filter_query)  # Updating the entry based on the filter
+            result = db_collection.delete_one(
+                filter_query
+            )  # Updating the entry based on the filter
 
             return result
 
         except Exception as e:
-            print(f'Error while deleting an entry from the DB.', str(e))
+            print(f"Error while deleting an entry from the DB.", str(e))
 
         return None
 
@@ -175,11 +206,13 @@ class DatabaseActions:
         """Deletes the data on a given key"""
         try:
             db_collection = self.db_connector.db[collection]
-            result = db_collection.delete_many(filter_query)  # Updating the entry based on the filter
+            result = db_collection.delete_many(
+                filter_query
+            )  # Updating the entry based on the filter
 
             return result
 
         except Exception as e:
-            print(f'Error while deleting an entry from the DB.', str(e))
+            print(f"Error while deleting an entry from the DB.", str(e))
 
         return None
